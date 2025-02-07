f990840fd3c693e4d2d579f2aaf962dc51cbb947
Nienge David <davidnienge@gmail.com>
2025-02-07 10:59:27 +0100

    Update footer.ts

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -2,5 +2,5 @@ import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
   title: 'Design & Built by David Nienge',
-  link: 'https://github.com/NiengeDavid/tee-portfolio-website',
+  link: 'https://davidnienge.com.ng',
 };
